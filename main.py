16c1070de1a9d952935517dd74a79f00443f9e9f
gabrielseron <gabriel.seron.pro@gmail.com>
2021-12-13 15:00:18 +0100

    up

--- main.py
+++ main.py
@@ -19,20 +19,35 @@ def roll_dice_set(nb_dice_to_roll):
     return dice_value_occurrence_list
 
 
-print(roll_dice_set(6))
+def analyse_bonus_score(dice_value_occurrence_list):
+    score = 0
+    for side_value_index, dice_value_occurrence in enumerate(dice_value_occurrence_list):
+        nb_of_bonus = dice_value_occurrence // THRESHOLD_BONUS
+        if nb_of_bonus > 0:
+            if side_value_index == 0:
+                bonus_multiplier = ACE_BONUS_MULTIPLIER
+            else:
+                bonus_multiplier = STD_BONUS_MULTIPLIER
+            score += nb_of_bonus * bonus_multiplier * (side_value_index + 1)
+            dice_value_occurrence_list[side_value_index] %= THRESHOLD_BONUS
+
+    return score, dice_value_occurrence_list
 
 
-def roll_dice_score_standard(dice_value_occurrence_list):
-    dice_score = 0
-    for n in range(len(dice_value_occurrence_list)):
-        for i in range(len(SCORING_DICE_VALUE_LIST)):
-            if dice_value_occurrence_list[n] == SCORING_DICE_VALUE_LIST[i]:
-                dice_score += SCORING_MULTIPLIER_LIST[i]
+def analyse_standard_score(dice_value_occurrence_list):
+    score = 0
+    for scoring_value, scoring_multiplier in zip(SCORING_DICE_VALUE_LIST, SCORING_MULTIPLIER_LIST):
+        score += dice_value_occurrence_list[scoring_value - 1] * scoring_multiplier
+        dice_value_occurrence_list[scoring_value - 1] = 0
 
-        return dice_score
-
+    return score, dice_value_occurrence_list
 
 
-roll_list =  roll_dice_set(6)
-print (roll_list)
-print(roll_dice_score_standard(roll_list))
+def analyse_score(dice_value_occurrence_list):
+    bonus_score, dice_value_occurrence_list = analyse_bonus_score(dice_value_occurrence_list)
+    standard_score, dice_value_occurrence_list = analyse_standard_score(dice_value_occurrence_list)
+
+    return bonus_score + standard_score, dice_value_occurrence_list
+
+
+print(analyse_score([7, 2, 3, 0, 4, 1]))
